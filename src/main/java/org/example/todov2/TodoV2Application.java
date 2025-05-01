3737d229f91970f71ed790cb20fddbdd8a4341fd
pleasebelieveme <cain1227@naver.com>
2025-05-01 11:15:17 +0900

    init: 디비연결, gitignore 설정

---
+++ src/main/java/org/example/todov2/TodoV2Application.java
@@ -0,0 +1,13 @@
+package org.example.todov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoV2Application.class, args);
+	}
+
+}
